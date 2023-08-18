2cd874cc6373cf7a49ae8345ae8283fa9c38c5a6
Vera Deeva <verasdee@gmail.com>
2023-08-18 13:41:09 +0700

    add next and prev pagination

--- script/pagination.js
+++ script/pagination.js
@@ -4,32 +4,69 @@ const listArticles = document.querySelector('[data-page]');
 console.log('listArticles: ', listArticles);
 // const pages = document.querySelectorAll('#pagination li');
 // console.log('pages: ', pages);
+
 const listPages = document.querySelector('#pagination');
+const prev = document.querySelector('.footer__btn-previous');
+const next = document.querySelector('.footer__btn-next');
 
-let articlesOnPage = 3;
-
-let countOfPages = Math.ceil(meta.pagination.total / meta.pagination.pages);
-console.log('countOfPages: ', countOfPages);
+let articlesOnPage = 4;
+// let articlesOnPage = meta.pagination.limit;
+let totalPages = meta.pagination.total;
+let numberOfPages = Math.ceil(totalPages / articlesOnPage);
+console.log('numberOfPages: ', numberOfPages);
+let currentPage = 1;
 
 let pages = [];
-for (let i = 1; i <= 3; i++) {
-    let page = document.createElement('li');
-    page.classList.add('footer__page');
-    page.textContent = i;
-    listPages.append(page);
-    pages.push(page);
+function showPagination(currentPage) {
+    listPages.textContent = '';
+    console.log(listPages);
+    for (let i = currentPage; i < currentPage + 3; i++) {
+        let page = document.createElement('li');
+        page.classList.add('footer__page');
+        page.textContent = i;
+        listPages.append(page);
+        pages.push(page);
+    };
 };
 
-let active;
+showPagination(currentPage);
+showPage(pages[0]);
+
+
+
 for (const item of pages) {
     item.addEventListener('click', function () {
         showPage(this);
-
     });
 };
 
+next.addEventListener('click', () => {
+    if (currentPage === totalPages) {
+        showPagination(currentPage);
+        next.style.cssText = `
+        display: none;
+        `;
+    } else {
+        currentPage++;
+        showPagination(currentPage);
+    }
+});
+
+prev.addEventListener('click', () => {
+    if (currentPage === 1) {
+        showPagination(currentPage);
+        prev.style.cssText = `
+        display: none;
+        `;
+    } else {
+        currentPage--;
+        showPagination(currentPage);
+    }
+});
+
 function showPage(item) {
     console.log('this.innerHTML: ', item.innerHTML);
+    let active = document.querySelector('#pagination li.active');
     if (active) {
         active.classList.remove('active');
     };
@@ -39,13 +76,13 @@ function showPage(item) {
     let startPage = (pageNum - 1) * articlesOnPage;
     let endPage = startPage + articlesOnPage;
 
-    let articles = data.slice(startPage, endPage);
-    console.log('articles', articles);
+    let paginatedArticles = data.slice(startPage, endPage);
+    console.log('paginatedArticles', paginatedArticles);
 
     listArticles.textContent = '';
 
     function createListArticles() {
-        for (const article of articles) {
+        for (const article of paginatedArticles) {
             let blogLink = document.createElement('a');
             blogLink.classList.add('blog__link');
             blogLink.href = `article.html`;
